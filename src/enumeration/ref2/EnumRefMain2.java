457e617381ce4450686c8fef5fac19214c8967d7
junmin <coom1222@gmail.com>
2025-08-26 20:01:53 +0900

    java-mid1 50강

---
+++ src/enumeration/ref2/EnumRefMain2.java
@@ -0,0 +1,18 @@
+package enumeration.ref2;
+
+
+public class EnumRefMain2 {
+
+    public static void main(String[] args) {
+        int price = 10000;
+
+        DiscountService discountService = new DiscountService();
+        int basic = discountService.discount(Grade.BASIC, price);
+        int gold = discountService.discount(Grade.GOLD, price);
+        int diamond = discountService.discount(Grade.DIAMOND, price);
+
+        System.out.println("basic 할인금액 " + basic);
+        System.out.println("gold 할인금액 " + gold);
+        System.out.println("diamond 할인금액 " + diamond);
+    }
+}
--- src/enumeration/ref2/Grade.java
+++ src/enumeration/ref2/Grade.java
@@ -1,7 +1,16 @@
 package enumeration.ref2;
 
 public enum Grade {
-    BASIC, GOLD, DIAMOND
+    BASIC(10), GOLD(20), DIAMOND(30);
 
+    private final int discountPercent;
 
+    // ebum 생성자는 묵시적으로 private이다
+    Grade(int discountPercent) {
+        this.discountPercent = discountPercent;
+    }
+
+    public int getDiscountPercent() {
+        return discountPercent;
+    }
 }
