9fb6d7de658da42d168d24a64952dc9b7d2fae1e
junmin <coom1222@gmail.com>
2025-08-26 19:55:35 +0900

    java-mid1 49강

---
+++ src/enumeration/ref1/ClassGradeRefMain1.java
@@ -0,0 +1,18 @@
+package enumeration.ref1;
+
+
+public class ClassGradeRefMain1 {
+
+    public static void main(String[] args) {
+        int price = 10000;
+
+        DiscountService discountService = new DiscountService();
+        int basic = discountService.discount(ClassGrade.BASIC, price);
+        int gold = discountService.discount(ClassGrade.GOLD, price);
+        int diamond = discountService.discount(ClassGrade.DIAMOND, price);
+
+        System.out.println("basic 할인금액 " + basic);
+        System.out.println("gold 할인금액 " + gold);
+        System.out.println("diamond 할인금액 " + diamond);
+    }
+}
---
+++ src/enumeration/ref2/Grade.java
@@ -0,0 +1,7 @@
+package enumeration.ref2;
+
+public enum Grade {
+    BASIC, GOLD, DIAMOND
+
+
+}
