050f4fd4e85c1337c3a97dc81ca8a9e3c7ded6da
junmin <coom1222@gmail.com>
2025-08-27 23:58:39 +0900

    java-mid1 53강

--- src/enumeration/test/AuthGradeMain2.java
+++ src/enumeration/test/AuthGradeMain2.java
@@ -7,7 +7,7 @@ public class AuthGradeMain2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("당신의 등급을 입력하세요[GUEST, MEMBER, ADMIN]: ");
+        System.out.println("당신의 등급을 입력하세요[GUEST, LOGIN, ADMIN]: ");
         String grade = scanner.nextLine();
 
 
