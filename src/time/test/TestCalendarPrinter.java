cc3faaa5e2cbb4aac96e123e6ed480f6cdf51399
junmin <coom1222@gmail.com>
2025-09-11 22:45:02 +0900

    java-mid1 66강

--- src/time/test/TestCalendarPrinter.java
+++ src/time/test/TestCalendarPrinter.java
@@ -15,16 +15,16 @@ public class TestCalendarPrinter {
         System.out.print("월 입력하세요: ");
         int month = scanner.nextInt();
 
+        // year, month를 받는 메소드를 만들어 보자.
         printCalendar(year, month);
 
-        //
     }
 
     public static void printCalendar(int year, int month) {
-        LocalDate firstDayOfMonth = LocalDate.of(year, month, 1);
-        LocalDate firstDayOfNextMonth = firstDayOfMonth.plusMonths(1);
+        LocalDate firstDayOfMonth = LocalDate.of(year, month, 1);        // 달력의 시작
+        LocalDate firstDayOfNextMonth = firstDayOfMonth.plusMonths(1);  // 달력의 끝
 
-        // 일요일이 0, 월요일이 1, ..., 토요일이 6인 값을 반환
+        // 일요일=0, 월요일=1, ..., 일요일=7인 값을 반환
         int offsetWeekDays = firstDayOfMonth.getDayOfWeek().getValue() % 7;
 
         System.out.println("일\t월\t화\t수\t목\t금\t토");
@@ -33,14 +33,16 @@ public class TestCalendarPrinter {
             System.out.print("\t"); // offsetWeekDays 만큼 빈칸 출력
         }
 
+        // 1일부터 다음달 첫날의 전날까지(이번달 말까지) 반복문을 돌리자
         LocalDate dayIterator = firstDayOfMonth;
-
-        while (dayIterator.isBefore(firstDayOfNextMonth)) { // 다음달의 첫날 전날까지
+        while (dayIterator.isBefore(firstDayOfNextMonth)) {
             System.out.print(dayIterator.getDayOfMonth() + "\t");
+            // System.out.printf("%2d", dayIterator.getDayOfMonth()); %2d는 2자리 공간 확보, 오른쪽 정렬
 
-            // getDayOfWeek() : 주어진 날짜가 무슨 요일인지
+            // getDayOfWeek() : 주어진 날짜가 무슨 요일인지 확인하는 메서드
+            // 이넘타입이므로 ==으로 비교 가능, 토요일마다 엔터 추가
             if (dayIterator.getDayOfWeek() == DayOfWeek.SATURDAY) {
-                System.out.println(); // 토요일이면 줄바꿈
+                System.out.println();
             }
 
             dayIterator = dayIterator.plusDays(1); // 다음날로 이동
