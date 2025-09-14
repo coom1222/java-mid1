f944cc540c377d072ffb3c233f3e8ccea63c1bb2
junmin <coom1222@gmail.com>
2025-09-14 20:39:23 +0900

    java-mid1 72강

---
+++ src/nested/nested/inner/ex2/CarMain.java
@@ -0,0 +1,9 @@
+package nested.nested.inner.ex2;
+
+public class CarMain {
+
+    public static void main(String[] args) {
+        Car myCar = new Car("Model3", 100);
+        myCar.start();
+    }
+}
