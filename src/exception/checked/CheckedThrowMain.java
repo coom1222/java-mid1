ce7590ece78d4d626ace8a793bc3f1cbe9c6361e
junmin <coom1222@gmail.com>
2025-12-24 15:48:04 +0900

    java-mid1 90강 - 체크 예외

---
+++ src/exception/checked/CheckedThrowMain.java
@@ -0,0 +1,10 @@
+package exception.checked;
+
+public class CheckedThrowMain {
+
+    public static void main(String[] args) throws MyCheckedException {
+        Service service = new Service();
+        service.catchThrow();   // 여기서 예외를 메인 밖으로 던졌기에 정상종료를 출력하지 못하고 끝난다.
+        System.out.println("정상종료");
+    }
+}
---
+++ src/exception/checked/Client.java
@@ -0,0 +1,12 @@
+package exception.checked;
+
+public class Client {
+    public void call() throws MyCheckedException {
+        // 문제 상황
+        throw new MyCheckedException("ex");
+    }
+}
+/**
+ * throw 예외라고 하면 새로운 예외를 발생시킬 수 있다. 예외도 객체이기 때문에 객체를 먼저 new로 생성하고 예외를 발생시킨다.
+ * throws 예외는 발생시킨 예외를 메서드 밖으로 던질 때 사용하는 키워드이다.
+ */
