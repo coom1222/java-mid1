ac19728b28deeb43033195fa9883f8d604473e10
junmin <coom1222@gmail.com>
2026-01-09 16:01:01 +0900

    java-mid1 96강: 예외처리 도입5 - finally

---
+++ src/exception/ex2/NetworkServiceV2_5.java
@@ -0,0 +1,27 @@
+package exception.ex2;
+
+
+public class NetworkServiceV2_5 {
+
+    public void sendMassage(String data) {// 이러면 메인에서 처리해야할것
+        String address = "http://example.com";
+        NetworkClientV2 client = new NetworkClientV2(address);
+        client.initError(data);
+
+        try {   // 하나의 try안에 정상 흐름을 모두 담는다.
+            client.connect();
+            client.send(data);
+        } catch (NetworkClientExceptionV2 e) {  // 예외 흐름은 캐치 블럭에서 해결한다.
+            System.out.println("[오류] 코드: " + e.getErrorCode() + ", 메시지: " + e.getMessage());
+        } finally {
+            client.disconnect();
+        }
+    }
+}
+/**
+ * tyy ~ catch ~ finally 구조는 정상흐름, 예외흐름, 마무리 흐름(반드시 호출해야 하는)을 제공한다.
+ * try를 시작하기만 하면, finally 코드 블럭은 어떤 경우라도 반드시 호출된다.
+ * try, catch 안에서 잡을 수 없는 예외가 발생해도 finally는 반드시 호출된다.
+ */
+
+
